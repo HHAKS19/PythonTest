a83b470836ab22316cc4d0ab2399b701a7e19a4e
HHAKS19 <htethtetayekhaingsoe1912@gmail.com>
2022-04-02 23:11:52 +0630

    feat: Star Pattern

---
+++ Star/Pattern_1.py
@@ -0,0 +1,10 @@
+n = int(input('Type a number:'))
+for i in range(n):
+    for j in range(i):
+        print('*',end = ' ')
+    print()
+for i in range(n):
+    for j in range(i,n):
+        print('*',end = ' ')
+    print()
+
