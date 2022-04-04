301e4da13e0ceca0f85d6e7156b766b66b5e3d1f
HHAKS19 <htethtetayekhaingsoe1912@gmail.com>
2022-04-04 21:36:16 +0630

    feat: Palindrome Check

---
+++ Algorithm/String/IsPalindromeString.py
@@ -0,0 +1,23 @@
+def CheckPalin(string):
+    reversed_str = string[::-1]
+    if string != reversed_str:
+        print('Sorry.It\'s not palindrome')
+    else:
+        print('Yes.{} is palindrome'.format(string))
+string = input('Enter any string: ')
+CheckPalin(string)
+
+# def check_palindrome_1(string):
+#     reversed_string = string[::-1]
+#     status=1
+#     if(string!=reversed_string):
+#         status=0
+#     return status
+
+
+# string = input("Enter the string: ")
+# status= check_palindrome_1(string)
+# if(status):
+#     print("It is a palindrome ")
+# else:
+#     print("Sorry! Try again")
